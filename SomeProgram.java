498217e1bb093b192b509eaf362f7f21f3a5d63f
Bartlomiej Mikler <bmikler@student.agh.edu.pl>
2022-01-23 11:31:57 +0100

    managing-rockets

--- SomeProgram.java
+++ SomeProgram.java
@@ -1,6 +1,12 @@
 public class SomeProgram { 
-    public static void main(String[] args) {
-      System.out.println("We are learning to use Git.");
-	 System.out.println("We will be Git experts, soon.");
-    }
+	public static void main(String[] args) {
+		System.out.println("We are learning to use Git."){
+		System.out.println("We will be Git experts, soon.");
+	}
+	
+	public static void manageRockets() {
+	
+		//TODO implements
+	}
+
 }
